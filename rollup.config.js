edab564c9bb5de804440f1412cd21ac9ee6a8839
Rudy Huynh <rudyhuynh@niometrics.com>
2020-05-29 23:19:14 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,27 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 import pkg from "./package.json";
+import packageJson from "./package.json";
+
+const { name, version, dependencies = {}, peerDependencies = {} } = packageJson;
 
 const input = "src/main.js";
+const external = Object.keys(Object.assign({}, dependencies, peerDependencies));
+const banner = `// ${name}@${version} - ${new Date().toISOString()}`;
 
 export default [
   // browser-friendly UMD build
   {
     input,
+    external,
     output: {
       name: "useUrlSearchParams",
       file: pkg.browser,
       format: "umd",
+      banner,
+      globals: {
+        react: "React",
+      },
     },
     plugins: [
       resolve(),
@@ -31,9 +41,10 @@ export default [
   // `file` and `format` for each target)
   {
     input,
+    external,
     output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
+      { file: pkg.main, format: "cjs", banner },
+      { file: pkg.module, format: "es", banner },
     ],
     plugins: [
       babel({
