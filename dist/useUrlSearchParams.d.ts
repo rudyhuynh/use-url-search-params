598e4c7a21260a94a6253a83627670ae2b2309a9
bryneTane <bftane@gmail.com>
2021-02-01 15:25:37 +0100

    update

--- dist/useUrlSearchParams.d.ts
+++ dist/useUrlSearchParams.d.ts
@@ -11,10 +11,12 @@ declare module "use-url-search-params" {
    *
    * @param initial initial value for query object
    * @param types help to resolve value from URL query before map to query object
+   * @param replace wether URL is replaced or not
    * @returns `[queries, setQueries]`
    */
   export function useUrlSearchParams(
     initial: InitialType,
-    types: TypesType
+    types: TypesType,
+    replace: boolean
   ): [InitialType, (nextQuery: InitialType) => void];
 }
